7c171c6963f1677db7dd932eddf8029a1275a9ac
Khalil H <81334438+Khalil-Hawari@users.noreply.github.com>
2023-07-04 19:08:31 -0400

    Changing environment to a new venv I'm using

---
+++ testing-venv/sklearn-test/RD-iris.py
@@ -0,0 +1,34 @@
+### Multiclass Classification using Random Forest on Scikit-Learn Library
+
+#Importing Libraries
+import numpy as np
+np.__version__
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+# from sklearn.externals import joblib
+import joblib
+import os
+
+cwd = os.getcwd()
+iris_file =  os.sep.join([cwd, 'sklearn-test', 'iris', 'iris.data.csv'])
+print('cwd:', iris_file)
+
+#Creating Dataset and including the first row by setting no header as input
+dataset = pd.read_csv(iris_file, header = None)
+#Renaming the columns
+dataset.columns = ['sepal length in cm', 'sepal width in cm','petal length in cm','petal width in cm','species']
+print('Shape of the dataset: ' + str(dataset.shape))
+print(dataset.head())
+
+
+
+#Creating the dependent variable class
+# All this really does is convert species names to numbers, since SK cannot work with strings
+factor = pd.factorize(dataset['species'])
+dataset.species = factor[0]
+definitions = factor[1]
+print(dataset.species.head())
+print(definitions)
